ba7807045142d58a56f9a673fbcd2110b28857e1
Matt <berrmal@gmail.com>
2016-01-23 12:11:46 -0800

    polysum passes tests, v1.0 complete

--- polysum.py
+++ polysum.py
@@ -3,22 +3,26 @@ Polysum takes 2 arguments:
     num_sides = integer number of sides of a regular polygon
     len_sides = float length of each side of a regular polygon
 and returns the sum of the area and the square of the perimeter
-as a float, rounded to 4 decimal places.
+as a float, rounded to 4 decimal places, eg:
+
 >>> polysum(4,4)
-16.0
+272.0
+>>> polysum(8,19.5)
+26172.0094
+>>> polysum(7,2.333)
+286.4805
 '''
 from math import tan, pi
 
 def polysum(num_sides, len_sides):
-    poly_sum = poly_area(num_sides, len_sides) + 
-                   poly_perim_square(num_sides, len_sides)
+    poly_sum = _poly_area(num_sides, len_sides) + _poly_perim_square(num_sides, len_sides)
     return round(poly_sum, 4)
 
 def _poly_area(num_sides, len_sides):
-    (0.25*num_sides*len_sides**2)/tan(pi/num_sides)
+    return (0.25*num_sides*len_sides**2)/tan(pi/num_sides)
 
 def _poly_perim_square(num_sides, len_sides):
-    return num_sides*len_sides**2
+    return (num_sides*len_sides)**2
 
 if __name__ == '__main__':
     import doctest
