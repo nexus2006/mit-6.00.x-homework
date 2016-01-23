0707c87b1ffd52e92acb6fa24500294f111d3508
Matt <berrmal@gmail.com>
2016-01-23 12:02:44 -0800

    untested final draft of polysum

--- polysum.py
+++ polysum.py
@@ -12,7 +12,7 @@ from math import tan, pi
 def polysum(num_sides, len_sides):
     poly_sum = poly_area(num_sides, len_sides) + 
                    poly_perim_square(num_sides, len_sides)
-    return poly_sum
+    return round(poly_sum, 4)
 
 def _poly_area(num_sides, len_sides):
     (0.25*num_sides*len_sides**2)/tan(pi/num_sides)
